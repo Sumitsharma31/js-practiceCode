a9591986cbca6a402cf39e5a0d72e5082f4a4286
sumit sharma <ssumit20kr@gmail.com>
2025-04-15 21:32:41 +0530

    currencyAPINot Working

--- 06currencyConvertor/src/hooks/useCurrencyInfo.js
+++ 06currencyConvertor/src/hooks/useCurrencyInfo.js
@@ -2,9 +2,10 @@ import {useEffect, useState} from "react"
 
 
 function useCurrencyInfo(currency){
+
     const [data, setData] = useState({})
     useEffect(() => {
-        fetch(`https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`)
+        fetch(`API PRIVATE KEY`)
         .then((res) => res.json())
         .then((res) => setData(res[currency]))
         console.log(data);
