162584885ba5d11daae49e6c491f4a6ef847375c
sumit sharma <ssumit20kr@gmail.com>
2025-05-05 22:14:24 +0530

    apply method in js

--- js-questions/thisKeyWord.js
+++ js-questions/thisKeyWord.js
@@ -29,4 +29,16 @@ function sayHello(){
   var obj = {name: "Sandy"};
           
   sayHello.call(obj);
+
+
+//   2.apply()
+
+/*The apply method is similar to the call() method. The only difference is that,
+
+call() method takes arguments separately whereas, apply() method takes arguments as an array.*/
           
+function saySomething(message){
+    return this.name + " is " + message;
+  }        
+  var person4 = {name:  "John"};
+  saySomething.apply(person4, ["awesome"]);
