99832c3879ce5806cb9b6b1dd260ebb458aefbb5
sumit sharma <ssumit20kr@gmail.com>
2025-05-05 22:16:53 +0530

    bind method in js

--- js-questions/thisKeyWord.js
+++ js-questions/thisKeyWord.js
@@ -42,3 +42,24 @@ function saySomething(message){
   }        
   var person4 = {name:  "John"};
   saySomething.apply(person4, ["awesome"]);
+
+//   2. bind()
+
+/*This method returns a new function, where the value of “this” keyword will be bound to the owner object, which is provided as a parameter.*/
+
+var bikeDetails = {
+    displayDetails: function(registrationNumber,brandName){
+    return this.name+ " , "+ "bike details: "+ registrationNumber + " , " + brandName;
+  }
+}
+   
+var person1 = {name:  "Vivek"};
+     
+var detailsOfPerson1 = bikeDetails.displayDetails.bind(person1, "TS0122", "Bullet");
+      
+// Binds the displayDetails function to the person1 object
+        
+      
+detailsOfPerson1();
+//Returns Vivek, bike details: TS0122, Bullet
+
