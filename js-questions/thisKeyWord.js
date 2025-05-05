8077f8beb2c9a53ffd446384f66c6349dab3c4f4
sumit sharma <ssumit20kr@gmail.com>
2025-05-05 22:11:07 +0530

    call Function in js

--- js-questions/thisKeyWord.js
+++ js-questions/thisKeyWord.js
@@ -4,11 +4,29 @@
      
 //   doSomething();
 
-let obj = {
-    name:  "Sumit",
-    getName: function(){
-    console.log(this.name);
-  }
-}
+// let obj = {
+//     name:  "Sumit",
+//     getName: function(){
+//     console.log(this.name);
+//   }
+// }
    
-obj.getName();
+// obj.getName();
+
+
+
+// Explain call(), apply() and, bind() methods.
+
+
+// 1. call()
+/*It’s a predefined method in javascript.
+This method invokes a method (function) by specifying the owner object.*/
+
+function sayHello(){
+    return "Hello " + this.name;
+  }
+          
+  var obj = {name: "Sandy"};
+          
+  sayHello.call(obj);
+          
