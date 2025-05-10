3d02e878d6a08059ef824a2905a1806c5b4c46a9
sumit sharma <ssumit20kr@gmail.com>
2025-05-10 23:20:29 +0530

    spread operator in javascript

--- js-questions/jsmethods.js
+++ js-questions/jsmethods.js
@@ -1,26 +1,32 @@
-
-// let reverseStr = (str)=> {
-//    let rstr='';
-//     for (let i = str.length; i >=0 ; i--) {
-//         rstr+=str.charAt(i)
-        
-        
-//     }
-//     return rstr;
-    
-// }
-// let result=reverseStr('hello');
-// console.log(result);
+/*
+let reverseStr = (str) => {
+    let rstr = '';
+    for (let i = str.length; i >= 0; i--) {
+        rstr += str.charAt(i)
 
 
-//largest number of an array
+    }
+    return rstr;
 
-const array =[12,15,41,12,24,31,8];
+}
+let result = reverseStr('hello');
+console.log(result);
+*/
 
+
+/*
+// to find largest element of an array
+const array = [12, 15, 41, 12, 24, 31, 8];
 function findLargeNumberOfArray(array) {
-    
-const largest = array.reduce((max, current) => current > max ? current : max);
-return largest;
+
+    const largest = array.reduce((max, current) => current > max ? current : max);
+    return largest;
 
 }
 console.log(findLargeNumberOfArray(array));
+*/
+
+// Using Math.max and Spread Operator
+const array = [12, 15, 41, 12, 24, 31, 8];
+const largest = Math.max(...array);
+console.log("Largest number:", largest); 
