e010b2ebf6b998d6c7821c9e6a28ab21e318f8b0
sumit sharma <ssumit20kr@gmail.com>
2025-05-10 23:10:35 +0530

    largest element of an array

--- js-questions/jsmethods.js
+++ js-questions/jsmethods.js
@@ -1,13 +1,26 @@
 
-let reverseStr = (str)=> {
-   let rstr='';
-    for (let i = str.length; i >=0 ; i--) {
-        rstr+=str.charAt(i)
+// let reverseStr = (str)=> {
+//    let rstr='';
+//     for (let i = str.length; i >=0 ; i--) {
+//         rstr+=str.charAt(i)
         
         
-    }
-    return rstr;
+//     }
+//     return rstr;
     
+// }
+// let result=reverseStr('hello');
+// console.log(result);
+
+
+//largest number of an array
+
+const array =[12,15,41,12,24,31,8];
+
+function findLargeNumberOfArray(array) {
+    
+const largest = array.reduce((max, current) => current > max ? current : max);
+return largest;
+
 }
-let result=reverseStr('hello');
-console.log(result);
+console.log(findLargeNumberOfArray(array));
