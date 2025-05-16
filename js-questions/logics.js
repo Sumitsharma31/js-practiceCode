82d6e8b5fc7b3c8f40d5c66a866051e6bf466b0a
sumit sharma <ssumit20kr@gmail.com>
2025-05-16 21:15:00 +0530

    array Destructuring

--- js-questions/logics.js
+++ js-questions/logics.js
@@ -12,21 +12,30 @@
 // console.log("5" - 3-1);
 
 
-const a=1;
-const b=true;
+// const a=1;
+// const b=true;
 
 
 
-console.log(a==b);
-console.log(a===b);
+// console.log(a==b);
+// console.log(a===b);
 
 // console.log(2/false);
-console.log(NaN==NaN);
-const arr=[1,8,4,2,7]
+// console.log(NaN==NaN);
+// const arr=[1,8,4,2,7]
 
-const newArr=arr.map((e)=> e>2);
-console.log(newArr);
-arr.sort()
-arr.shift()
-arr.unshift(0,1)
-console.log(arr);
+// const newArr=arr.map((e)=> e>2);
+// console.log(newArr);
+// arr.sort()
+// arr.shift()
+// arr.unshift(0,1)
+// console.log(arr);
+
+
+// Array DeStructuring
+const array=["name1","name2"];
+const [n1,n2]=array;
+console.log(n1);
+console.log(n2);
+
+
