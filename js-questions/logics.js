70d9e6f08257021420787f782aa6f0b660825ceb
sumit sharma <ssumit20kr@gmail.com>
2025-05-16 20:51:42 +0530

    interview Questions Practice

--- js-questions/logics.js
+++ js-questions/logics.js
@@ -1,13 +1,32 @@
 // JS questions 
-console.log([] + []);
+// console.log([] + []);
 
 
 
-console.log(0.1 + 0.2 === 0.3);
+// console.log(0.1 + 0.2 === 0.3);
 
 
-console.log(typeof null);
+// console.log(typeof null);
 
 
-console.log("5" - 3-1);
+// console.log("5" - 3-1);
 
+
+const a=1;
+const b=true;
+
+
+
+console.log(a==b);
+console.log(a===b);
+
+// console.log(2/false);
+console.log(NaN==NaN);
+const arr=[1,8,4,2,7]
+
+const newArr=arr.map((e)=> e>2);
+console.log(newArr);
+arr.sort()
+arr.shift()
+arr.unshift(0,1)
+console.log(arr);
