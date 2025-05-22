0a746d825ffe5237dbe06536576e38ae473c3ed9
sumit sharma <ssumit20kr@gmail.com>
2025-05-22 23:24:14 +0530

    loops in js

--- js-questions/logics.js
+++ js-questions/logics.js
@@ -48,4 +48,4 @@ let c = { key: "c" };
 a[b] = 123;
 a[c] = 456;
 
-console.log(a[b]);
+console.log(a[b]);  //456
---
+++ loops/for-of.js
@@ -0,0 +1,6 @@
+let arrayIterable = [10, 20, 30, 40, 50];
+
+for (let value of arrayIterable) {
+  value++;
+  console.log(value); // 11 21 31 41 51
+}
