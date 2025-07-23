b09dc884c0c16d55d193973f95b96e94616ae77b
sumit sharma <ssumit20kr@gmail.com>
2025-07-23 15:49:44 +0530

    methods in BOM

--- js-BOM/script.js
+++ js-BOM/script.js
@@ -2,3 +2,10 @@ console.log('inner width'+window.innerWidth);
 console.log('outer width'+window.outerWidth);
 console.log('windows location'+window.location);
 console.log('screen'+window.screen);
+
+
+//some BOM methods 
+window.alert("alert message!");
+
+//location objects 
+console.log(location.href);
